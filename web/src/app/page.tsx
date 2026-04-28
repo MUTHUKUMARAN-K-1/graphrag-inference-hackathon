4adfc0319baf373c072682329de7096c240be675
muthukumaran k <muthuk1@users.noreply.huggingface.co>
2026-04-28 14:44:15 +0000

    🎨 New multi-section home page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,14 +1,24 @@
 import { Navbar } from "@/components/Navbar";
-import { Hero } from "@/components/Hero";
-import { DashboardTabs } from "@/components/DashboardTabs";
+import { HeroSection } from "@/components/home/HeroSection";
+import { StatsBar } from "@/components/home/StatsBar";
+import { FeaturesSection } from "@/components/home/FeaturesSection";
+import { HowItWorks } from "@/components/home/HowItWorks";
+import { BentoShowcase } from "@/components/home/BentoShowcase";
+import { TestimonialsSection } from "@/components/home/TestimonialsSection";
+import { CTASection } from "@/components/home/CTASection";
 import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <Hero />
-      <DashboardTabs />
+      <HeroSection />
+      <StatsBar />
+      <FeaturesSection />
+      <HowItWorks />
+      <BentoShowcase />
+      <TestimonialsSection />
+      <CTASection />
       <Footer />
     </main>
   );
